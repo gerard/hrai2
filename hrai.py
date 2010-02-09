6ea571ef139ba1f3af2f1394fdc0c3ae4ba97963
Gerard Lledo <gerard.lledo@gmail.com>
2010-02-09 23:02:22 +0200

    Fix hrai wrapper so it doesn't cut the parameters

--- hrai.py
+++ hrai.py
@@ -3,20 +3,25 @@
 import sys
 import os
 
+def run(bin):
+    if os.path.exists(bin):
+        cmd_line = bin
+        for param in sys.argv[2:]:
+            cmd_line += ' "' + param + '"'
+        sys.exit(os.system(cmd_line))
+
+
 try:
     cmd = sys.argv[1]
 except IndexError:
     sys.exit("E: Usage: " + sys.argv[0] + " command [options]")
 
-cmd_bin = os.path.dirname(sys.argv[0]) + os.sep + cmd + ".py"
-if os.path.exists(cmd_bin):
-    os.system(cmd_bin + " " + ' '.join(sys.argv[2:]))
-    sys.exit(0)
+cmd_bin = [
+    os.path.dirname(sys.argv[0]) + os.sep + cmd + ".py",
+    os.path.dirname(sys.argv[0]) + os.sep + "../libexec" + os.sep + cmd + ".py"
+]
 
-cmd_bin = os.path.dirname(sys.argv[0]) + os.sep + "../libexec" \
-                                       + os.sep + cmd + ".py"
-if os.path.exists(cmd_bin):
-    os.system(cmd_bin + " " + ' '.join(sys.argv[2:]))
-    sys.exit(0)
+for cmd_bin_try in cmd_bin:
+    run(cmd_bin_try)
 
 sys.exit("E: Couldn't find hrai2 installation")
